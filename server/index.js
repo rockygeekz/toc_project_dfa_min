ff9e159c1a49dcac76d3468e7ae84c590c22a72e
Rakesh s <rakesh.s552004@gmail.com>
2024-12-12 20:27:12 +0530

    deploy error

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const dfaRoutes = require("./routes/dfaRoutes");
+const dfaRoutes = require("./Routes/dfaRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
